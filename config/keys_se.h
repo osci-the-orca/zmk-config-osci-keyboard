46cf566dda34aed9ae8271de149717e7c66c1790
osci-the-orca <88oscste@skola.boras.se>
2024-01-03 11:44:35 +0100

    asdf

--- config/keys_se.h
+++ config/keys_se.h
@@ -35,56 +35,56 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define SE_SECT GRAVE   // §
-#define SE_N1 N1        // 1
-#define SE_N2 N2        // 2
-#define SE_N3 N3        // 3
-#define SE_N4 N4        // 4
-#define SE_N5 N5        // 5
-#define SE_N6 N6        // 6
-#define SE_N7 N7        // 7
-#define SE_N8 N8        // 8
-#define SE_N9 N9        // 9
-#define SE_N0 N0        // 0
-#define SE_PLUS MINUS   // +
-#define SE_ACUT EQUAL   // ´ (dead)
+#define SE_SECT GRAVE // §
+#define SE_N1 N1      // 1
+#define SE_N2 N2      // 2
+#define SE_N3 N3      // 3
+#define SE_N4 N4      // 4
+#define SE_N5 N5      // 5
+#define SE_N6 N6      // 6
+#define SE_N7 N7      // 7
+#define SE_N8 N8      // 8
+#define SE_N9 N9      // 9
+#define SE_N0 N0      // 0
+#define SE_PLUS MINUS // +
+#define SE_ACUT EQUAL // ´ (dead)
 // Row 2
-#define SE_Q    Q    // Q
-#define SE_W    W    // W
-#define SE_E    E    // E
-#define SE_R    R    // R
-#define SE_T    T    // T
-#define SE_Y    Y    // Y
-#define SE_U    U    // U
-#define SE_I    I    // I
-#define SE_O    O    // O
-#define SE_P    P    // P
+#define SE_Q Q       // Q
+#define SE_W W       // W
+#define SE_E E       // E
+#define SE_R R       // R
+#define SE_T T       // T
+#define SE_Y Y       // Y
+#define SE_U U       // U
+#define SE_I I       // I
+#define SE_O O       // O
+#define SE_P P       // P
 #define SE_ARNG LBKT // Å
 #define SE_DIAE RBKT // ¨ (dead)
 // Row 3
-#define SE_A    A    // A
-#define SE_S    S    // S
-#define SE_D    D    // D
-#define SE_F    F    // F
-#define SE_G    G    // G
-#define SE_H    H    // H
-#define SE_J    J    // J
-#define SE_K    K    // K
-#define SE_L    L    // L
+#define SE_A A       // A
+#define SE_S S       // S
+#define SE_D D       // D
+#define SE_F F       // F
+#define SE_G G       // G
+#define SE_H H       // H
+#define SE_J J       // J
+#define SE_K K       // K
+#define SE_L L       // L
 #define SE_ODIA SEMI // Ö
 #define SE_ADIA SQT  // Ä
 #define SE_QUOT NUHS // '
 // Row 4
 #define SE_LABK NUBS  // <
-#define SE_Z    Z     // Z
-#define SE_X    X     // X
-#define SE_C    C     // C
-#define SE_V    V     // V
-#define SE_B    B     // B
-#define SE_N    N     // N
-#define SE_M    M     // M
+#define SE_Z Z        // Z
+#define SE_X X        // X
+#define SE_C C        // C
+#define SE_V V        // V
+#define SE_B B        // B
+#define SE_N N        // N
+#define SE_M M        // M
 #define SE_COMM COMMA // ,
-#define SE_DOT  DOT   // .
+#define SE_DOT DOT    // .
 #define SE_MINS FSLH  // -
 
 /* Shifted symbols
@@ -101,19 +101,19 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define SE_HALF LS(SE_SECT)  // ½
-#define SE_EXLM LS(SE_N1)    // !
-#define SE_DQUO LS(SE_N2)    // "
-#define SE_HASH LS(SE_N3)    // #
-#define SE_CURR LS(SE_N4)    // ¤
-#define SE_PERC LS(SE_N5)    // %
-#define SE_AMPR LS(SE_N6)    // &
-#define SE_SLSH LS(SE_N7)    // /
-#define SE_LPRN LS(SE_N8)    // (
-#define SE_RPRN LS(SE_N9)    // )
-#define SE_EQL  LS(SE_N0)    // =
-#define SE_QUES LS(SE_PLUS)  // ?
-#define SE_GRV  LS(SE_ACUT)  // ` (dead)
+#define SE_HALF LS(SE_SECT) // ½
+#define SE_EXLM LS(SE_N1)   // !
+#define SE_DQUO LS(SE_N2)   // "
+#define SE_HASH LS(SE_N3)   // #
+#define SE_CURR LS(SE_N4)   // ¤
+#define SE_PERC LS(SE_N5)   // %
+#define SE_AMPR LS(SE_N6)   // &
+#define SE_SLSH LS(SE_N7)   // /
+#define SE_LPRN LS(SE_N8)   // (
+#define SE_RPRN LS(SE_N9)   // )
+#define SE_EQL LS(SE_N0)    // =
+#define SE_QUES LS(SE_PLUS) // ?
+#define SE_GRV LS(SE_ACUT)  // ` (dead)
 // Row 2
 #define SE_CIRC LS(SE_DIAE) // ^ (dead)
 // Row 3
@@ -138,17 +138,17 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define SE_AT   RA(SE_N2)    // @
-#define SE_PND  RA(SE_N3)    // £
-#define SE_DLR  RA(SE_N4)    // $
-#define SE_EURO RA(SE_N5)    // €
-#define SE_LCBR RA(SE_N7)       // {
-#define SE_LBRC RA(SE_N8)    // [
-#define SE_RBRC RA(SE_N9)    // ]
-#define SE_RCBR RA(SE_N0)    // }
+#define SE_AT RA(SE_N2)     // @
+#define SE_PND RA(SE_N3)    // £
+#define SE_DLR RA(SE_N4)    // $
+#define SE_EURO RA(SE_N5)   // €
+#define SE_LCBR RA(SE_N7)   // {
+#define SE_LBRC RA(SE_N8)   // [
+#define SE_RBRC RA(SE_N9)   // ]
+#define SE_RCBR RA(SE_N0)   // }
 #define SE_BSLS RA(SE_PLUS) // (backslash)
 // Row 2
 #define SE_TILD RA(SE_DIAE) // ~ (dead)
 // Row 4
 #define SE_PIPE RA(SE_LABK) // |
-#define SE_MICR RA(SE_M)    // µ 
+#define SE_MICR RA(SE_M)    // µ
